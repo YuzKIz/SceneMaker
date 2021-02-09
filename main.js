b308201db6a02d57f158ca12480da08762823314
YuzKIz <78481556+YuzKIz@users.noreply.github.com>
2021-02-10 00:08:51 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function menu(){
+  toWrite = '<div class="menuBar">'
+    + '<ul>'
+    + '<li><a href="javascipt:void(0);">メニュー画像<span class="sideBar"></span></a></li>'
+    + '</ul'>
+    + '</div>;
+  getElementById("header").innerHTML = toWrite;
+}
