d5e3b7ab2b9950ac23cfa25dbf9891ecf2b09869
YuzKIz <78481556+YuzKIz@users.noreply.github.com>
2021-02-10 21:27:25 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 function menu(){
   toWrite = '<div class="menuBar"><ul>'
-    + '<li><a href="javascipt:void(0);">メニュー画像<span class="sideBar"></span></a></li>'
+    + '<li><a href="javascipt:void(0);">メニュー画像<span class="sideBar"></span></a>'
     + '<div><ul>'
       + '<li><a href="javascript:void(0);">作品新規作成</a></li>'
       + '<li><a href="javascript:void(0);">ファイルから読み込み</a></li>'
       + '<li><a href="javascript:void(0);">ファイルに書き出し</a></li>'
     + '</ul></div>'
+    + '</li>'
     + '<li><a href="javascipt:void(0);">場面画像</a></li>'
     + '<li><a href="javascipt:void(0);">時間画像</a></li>'
     + '<li><a href="javascipt:void(0);">場所画像</a></li>'
