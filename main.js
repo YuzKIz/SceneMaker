edb8206b448391884f6841251c3412d205858a27
YuzKIz <78481556+YuzKIz@users.noreply.github.com>
2021-02-10 15:48:13 +0900

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function menu(){
   toWrite = '<div class="menuBar">'
     + '<ul>'
     + '<li><a href="javascipt:void(0);">メニュー画像<span class="sideBar"></span></a></li>'
-    + '</ul'>
-    + '</div>;
+    + '</ul>'
+    + '</div>';
   getElementById("header").innerHTML = toWrite;
 }
